943781a9179d16a223d627a46aeddae91ae5d5ef
MrHencke <hencke1@hotmail.com>
2021-04-15 19:39:51 +0200

    Fixed the mess

--- ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/middleware/ChordLookup.java
+++ ChordDHTPeer-Project3-Oppgave/src/no/hvl/dat110/middleware/ChordLookup.java
@@ -28,8 +28,7 @@ public class ChordLookup {
 	public NodeInterface findSuccessor(BigInteger key) throws RemoteException {
 
 		// ask this node to find the successor of key
-
-		NodeInterface keySuccessor = node.findSuccessor(key);
+		// NodeInterface keySuccessor = node.findSuccessor(key);
 
 		// get the successor of the node
 
@@ -37,35 +36,26 @@ public class ChordLookup {
 
 		// get the stub for this successor (Util.getProcessStub())
 
-		String nodeSuccessorName = nodeSuccessor.getNodeName();
-		int nodeSuccessorPort = nodeSuccessor.getPort();
-		NodeInterface nodeStub = Util.getProcessStub(nodeSuccessorName, nodeSuccessorPort);
+		NodeInterface nodeStub = Util.getProcessStub(nodeSuccessor.getNodeName(), nodeSuccessor.getPort());
 		//
 
 		// check that key is a member of the set {nodeid+1,...,succID} i.e. (nodeid+1 <=
 		// key <= succID) using the ComputeLogic
 
-		if (nodeStub != null) {
-
-			Boolean isMember = false;
-
-			BigInteger keySuccessorID = keySuccessor.getNodeID();
-			isMember = Util.computeLogic(key, node.getNodeID().add(new BigInteger("1")), keySuccessorID);
+		//if (nodeStub != null) {
 
 			// if logic returns true, then return the successor
-
-			if (isMember) {
+			if (Util.computeLogic(key, node.getNodeID().add(BigInteger.ONE), nodeStub.getNodeID())) {
 				return nodeSuccessor;
 			} else {
 				// if logic returns false; call findHighestPredecessor(key)
-				NodeInterface highest_pred = findHighestPredecessor(key);
 				// do return highest_pred.findSuccessor(key) - This is a recursive call until
 				// logic returns true
-				return highest_pred.findSuccessor(key);
+				return findHighestPredecessor(key).findSuccessor(key);
 			}
-		} else {
-			return null;
-		}
+		/*} else {
+			 return null;
+		}*/
 	}
 
 	/**
@@ -78,16 +68,24 @@ public class ChordLookup {
 	private NodeInterface findHighestPredecessor(BigInteger key) throws RemoteException {
 
 		// collect the entries in the finger table for this node
+		List<NodeInterface> ftable = node.getFingerTable();
 
 		// starting from the last entry, iterate over the finger table
+		for (int i = ftable.size() - 1; i >= 0; i--) {
+			// for each finger, obtain a stub from the registry
+			NodeInterface finger = ftable.get(i);
+			NodeInterface fStub = Util.getProcessStub(finger.getNodeName(), finger.getPort());
 
-		// for each finger, obtain a stub from the registry
+			// check that finger is a member of the set {nodeID+1,...,ID-1} i.e. (nodeID+1
+			// <= finger <= key-1) using the ComputeLogic
 
-		// check that finger is a member of the set {nodeID+1,...,ID-1} i.e. (nodeID+1
-		// <= finger <= key-1) using the ComputeLogic
+			// if logic returns true, then return the finger (means finger is the closest to
+			// key)
+			if (Util.computeLogic(fStub.getNodeID(), node.getNodeID().add(BigInteger.ONE), key.subtract(BigInteger.ONE))) {
+				return fStub;
+			}
 
-		// if logic returns true, then return the finger (means finger is the closest to
-		// key)
+		}
 
 		return (NodeInterface) node;
 	}
